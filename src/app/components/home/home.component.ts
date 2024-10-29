c91208ce50685c700f6799d830763520ee361970
danilopichilli <117393179+danilopichilli@users.noreply.github.com>
2024-10-29 22:09:11 +0100

    added home and footer components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonModule, CardModule, BadgeModule, ChipModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  codeExample = `Import in your pom.xml
 
+<dependency>
+    <groupId>io.github.jsoapdoc</groupId>
+    <artifactId>jsoapdoc-core</artifactId>
+    <version>0.0.3</version>
+</dependency>
+
+`;
+
+  features = [
+    {
+      title: 'Automated Documentation',
+      description:
+        'Generates detailed documentation for your SOAP services with minimal configuration.',
+      icon: 'pi pi-file',
+    },
+    {
+      title: 'SOAP 1.1 and 1.2 Compatibility',
+      description:
+        'Fully supports both SOAP 1.1 and 1.2 protocols, ensuring broad compatibility.',
+      icon: 'pi pi-cog',
+    },
+    {
+      title: 'Annotation-Based Setup',
+      description:
+        'Easily create documentation using annotations, simplifying integration with your codebase.',
+      icon: 'pi pi-tags',
+    },
+  ];
 }
